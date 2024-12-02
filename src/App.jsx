c6a8a6fe7440b89f6d3fa0cd89fbb3eae073cb5e
unknown <ellenxxy19@gmail.com>
2024-12-02 20:26:09 -0300

    Sidebar component done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { Post } from './Post';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+import { Post } from './Post';
 import './global.css';
+import styles from './App.module.css'
 
 export function App() {
 
   return (
     <div>
       <Header />
-      <Post 
-        author="Naelem Souza" 
-        conte
-        nt="lorem ipsum dolor sit amet" 
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Naelem Souza" 
+            conte
+            nt="lorem ipsum dolor sit amet" 
+          />
+        </main>
+      </div>
     </div>
   )
 }
