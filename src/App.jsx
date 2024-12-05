cabcb958c8dcb66fc9839bc995094c0c07535caa
naelem <ellenxxy19@gmail.com>
2024-12-05 18:02:02 -0300

    adding props to the post component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,41 @@ import { Post } from './components/Post';
 import styles from './App.module.css'
 import './global.css';
 
+//author: { avatar_url: "", name: "", role:""}
+//publishedAt: Date
+//content: string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://i.pinimg.com/736x/1e/b4/18/1eb418393750bff749feaa7f99e04559.jpg',
+      name: 'Mavie Becker',
+      role: 'Front-end Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},  
+    ],
+    publishedAt: new Date('2024-12-05 16:01'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://i.pinimg.com/736x/24/43/88/24438868e61acbb4bc9e8d4cd4f18621.jpg',
+      name: 'Josh Schneider',
+      role: 'Software Engineer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},  
+    ],
+    publishedAt: new Date('2024-12-03 16:01'),
+  },
+]
+
 export function App() {
 
   return (
@@ -15,8 +50,15 @@ export function App() {
         <Sidebar />
         
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
