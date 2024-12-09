99a099224847845fef2a59abf42d654967b3a880
naelem <ellenxxy19@gmail.com>
2024-12-09 15:03:32 -0300

    like counting feature added

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,14 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment (props) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1)
+    }
 
     function handleDeleteComment() {
         props.onDeleteComment(props.content);
@@ -29,9 +35,9 @@ export function Comment (props) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
